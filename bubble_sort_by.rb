5ebbf73737362289b941e4cfd5a0d5a9073556d1
inhaq <p126013@nu.edu.pk>
2018-12-21 15:50:10 +0500

    minor updates

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,20 +1,27 @@
-def bubble_sort_by arr
-	length = arr.count
-	index = length
+def bubble_sort_by(arr)
+  i = arr.length
+  n = arr.count
 
-	while index >= 0 do
-
-		for j in 0...index-1 do
-			a = yield(arr[j],arr[j+1])
-			if a > 0
-				arr[j],arr[j+1]=arr[j+1],arr[j]
-							else
-			end
-		end
-		index -= 1
-
-			end
-	arr
+  while i >= 0
+    for j in 0...n - 1
+      if block_given?
+        a = yield(arr[j], arr[j + 1])
+      else
+        a = arr[j + 1] <=> arr[j]
+      end
+      if a > 0
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+      end
+    end
+    i -= 1
+  end
+  arr
 end
 
-
+# works with the block
+output = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
+end
+# works without block - by default ascending
+output = bubble_sort_by(["hi", "hello", "hey"])
+p output
