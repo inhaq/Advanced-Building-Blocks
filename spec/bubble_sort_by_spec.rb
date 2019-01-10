8530ee0ad504e135d2e4f36bbb35fd834326bcee
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-10 11:01:34 +0100

    minor update

--- spec/bubble_sort_by_spec.rb
+++ spec/bubble_sort_by_spec.rb
@@ -1,14 +1,15 @@
 require_relative '../lib/bubble_sort_by.rb'
 
 describe Array do
-    describe '#bubble_sort_by' do
-        it 'returns sorted array' do
-            expect([5,4,3,2,1].bubble_sort_by).to eq([1,2,3,4,5])
-        end
+  let(:a) { [1, 2, 3, 4, 5] }
+  describe '#bubble_sort_by' do
+    it 'returns sorted array' do
+      expect([5, 4, 3, 2, 1].bubble_sort_by).to eq(a)
     end
-    describe '#bubble_sort_by with block' do
-        it 'returns sorted array with block given' do
-            expect([1,2,3,4,5].bubble_sort_by{|a,b| b-a }).to eq([5,4,3,2,1])
-        end
+  end
+  describe '#bubble_sort_by with block' do
+    it 'returns sorted array with block given' do
+      expect(a.bubble_sort_by { |a, b| b - a }).to eq([5, 4, 3, 2, 1])
     end
+  end
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,45 +1,45 @@
-require_relative "../lib/enumerable.rb"
+require_relative '../lib/enumerable.rb'
 
 describe Enumerable do
-    let(:a) {[1,2,3,4,5]}
-    describe "#my_count" do
-        it "gives size of array" do
-            expect(a.my_count).to eq(5)
-        end
+  let(:a) { [1, 2, 3, 4, 5] }
+  describe '#my_count' do
+    it 'gives size of array' do
+      expect(a.my_count).to eq(5)
     end
-    describe "#my_map" do
-        it 'returns changed array' do
-            expect(a.my_map{|x| x*2}).to eq([2,4,6,8,10])
-        end
+  end
+  describe '#my_map' do
+    it 'returns changed array' do
+      expect(a.my_map { |x| x * 2 }).to eq([2, 4, 6, 8, 10])
     end
-    describe "#my_select" do
-        it 'returns selected' do
-            expect(a.my_select{|x| x % 2 == 0}).to eq([2,4])
-        end
-    end 
-    describe "#my_any?" do
-        it 'returns true if any true' do
-            expect(a.my_any?{|x| x > 4}).to eq(true)
-        end
+  end
+  describe '#my_select' do
+    it 'returns selected' do
+      expect(a.my_select { |x| (x % 2).zero? }).to eq([2, 4])
     end
-    describe "#my_all?" do
-        it "return true if all true" do
-        expect(a.my_all?{|x| x % 2 == 0}).not_to eq(true)
-        end
+  end
+  describe '#my_any?' do
+    it 'returns true if any true' do
+      expect(a.my_any? { |x| x > 4 }).to eq(true)
     end
-    describe "#my_inject" do
-        it 'it reduces an array' do
-            expect(a.my_inject{|x,y| x+y}).to eq(15)
-        end
+  end
+  describe '#my_all?' do
+    it 'return true if all true' do
+      expect(a.my_all? { |x| (x % 2).zero? }).not_to eq(true)
     end
-    describe "#my_none?" do
-        it "returns true if all false" do
-            expect(a.my_none?{|x| x % 2 != 0}).to eq(false)
-        end
+  end
+  describe '#my_inject' do
+    it 'it reduces an array' do
+      expect(a.my_inject { |x, y| x + y }).to eq(15)
     end
-    describe "#multiply_els" do
-        it "multiples elements " do
-            expect(a.multiply_els{|x,y| x*y}).to eq(120)
-        end
+  end
+  describe '#my_none?' do
+    it 'returns true if all false' do
+      expect(a.my_none? { |x| (x % 2).zero? }).to eq(false)
     end
+  end
+  describe '#multiply_els' do
+    it 'multiples elements ' do
+      expect(a.multiply_els { |x, y| x * y }).to eq(120)
+    end
+  end
 end
